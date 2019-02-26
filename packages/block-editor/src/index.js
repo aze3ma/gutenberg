8d92e1ab5ac22dec24e10a46ae7b8e2a0fbb58e6
Riad Benguella <benguella@gmail.com>
2019-02-26 10:51:29 +0100

    Move the block components to the block editor module

--- packages/block-editor/src/index.js
+++ packages/block-editor/src/index.js
@@ -2,10 +2,13 @@
  * WordPress dependencies
  */
 import '@wordpress/blocks';
+import '@wordpress/rich-text';
+import '@wordpress/viewport';
 
 /**
  * Internal dependencies
  */
 import './store';
+import './hooks';
 
 export * from './components';
